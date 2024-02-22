8c718ada191843ed8de69e8658ad06931b016152
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-02-22 18:17:45 +0600

    ok

--- app/components/OurProject.jsx
+++ app/components/OurProject.jsx
@@ -3,6 +3,11 @@ import React, { useRef } from "react";
 import CountUp from "react-countup";
 import useIsInViewport from "../hooks/useIsInViewport";
 import Link from "next/link";
+import Image from "next/image";
+
+import img1 from '../../public/image 22.png';
+
+import Icon from '../../public/Group.png';
 
 const OurProject = () => {
   return (
@@ -38,8 +43,20 @@ const OurProject = () => {
         <div className="h-1 w-full max-w-[500px] bg-[#1B75BC]"></div>
       </div>
       <div className="flex">
-        <RivewCart></RivewCart>
-        <RivewCart></RivewCart>
+        <RivewCart img={img1}
+          name='Dr. Mazda Islam' 
+          post='American business owner' 
+          email='mazda@infotech.com' 
+          text1='Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim'
+          text2='Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim'
+          ></RivewCart>
+        <RivewCart img={img1}
+          name='Dr. Mazda Islam' 
+          post='American business owner' 
+          email='mazda@infotech.com' 
+          text1='Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim'
+          text2='Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim'
+          ></RivewCart>
       </div>
     </div>
   );
@@ -71,17 +88,39 @@ const SimpleCart = ({ number, title, description, count }) => {
       <div className="flex justify-center">
         <Link href='/' className="px-4 py-2 bg-white  text-black">View</Link>
       </div>
-      
+
     </div>
   );
 };
-const RivewCart = () => {
+const RivewCart = ({ img, name, post, email, text1, text2 }) => {
 
   return (
     <div
-      className=""
+      className=" shadow-md bg-[#1B75BC] rounded-md flex-col justify-left items-center flex-wrap p-6"
     >
-      review cart
+      <div className="flex-row">
+        <div className="flex-shrink-0">
+          <Image className="h-30 w-30 " src={img} alt='OurProject' />
+        </div>
+
+        <div className="ml-4">
+          <p className="font-bold text-lg">{name}</p>
+          <p className="">{post}</p>
+          <p className="">{email}</p>
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <p className="text-left">{text1}</p>
+      </div>
+
+      <div className="ml-auto flex items-center">
+        <div className="mr-2">
+          <Image className="h-30 w-30 " src={Icon} alt='OurProject' />
+        </div>
+
+        <p className="text-gray-600">{text2}</p>
+      </div>
     </div>
   );
 };
