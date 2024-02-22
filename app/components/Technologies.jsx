76cc912a13bb1a193b42606c28b88c87eb25934b
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-02-22 15:31:10 +0600

    ok

--- app/components/Technologies.jsx
+++ app/components/Technologies.jsx
@@ -1,15 +1,146 @@
-'use client'
+"use client";
+import Image from "next/image";
 import React from "react";
+import img2 from "../../public/img2.png";
 
 export default function Technologies() {
   return (
     <div className="md:px-10 px-2">
-      <div className="flex text-white justify-between">
+      <div className="md:flex text-white justify-between ">
         <div>
-          <p>Programming Languages</p>
+          <div>
+            <p className="mb-2">Programming Languages</p>
+            <div className="text-white flex flex-wrap gap-2 md:gap-5 border-t-2 border-t-[#1B75BC] py-4">
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p className="mb-2">Programming Languages</p>
+            <div className="text-white flex flex-wrap gap-2 md:gap-5 border-t-2 border-t-[#1B75BC] py-4">
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p className="mb-2">Programming Languages</p>
+            <div className="text-white flex flex-wrap gap-2 md:gap-5 border-t-2 border-t-[#1B75BC] py-4">
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p className="mb-2">Programming Languages</p>
+            <div className="text-white flex flex-wrap gap-2 md:gap-5 border-t-2 border-t-[#1B75BC] py-4">
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>Web Development Technologies</p>
+        <div className="md:mt-10 mt-5">
+          <div>
+            <p className="mb-2 md:text-end">Web Development Technologies</p>
+            <div className="text-white flex flex-wrap gap-2 md:gap-5 border-t-2 border-t-[#1B75BC] py-4 md:justify-end">
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p className="mb-2 md:text-end">Web Development Technologies</p>
+            <div className="text-white flex flex-wrap gap-2 md:gap-5 border-t-2 border-t-[#1B75BC] py-4 md:justify-end">
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+              <div className="">
+                <Image src={img2} alt="technology"></Image>
+                <p className="text-center ">Swift</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
