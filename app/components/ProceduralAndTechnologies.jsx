1fe2603b86905a21a7818eda29f9d4f6e3960513
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-02-24 01:54:43 +0600

    ok

--- app/components/ProceduralAndTechnologies.jsx
+++ app/components/ProceduralAndTechnologies.jsx
@@ -1,5 +1,16 @@
-function PreceduralAndTechnologies() {
-  return <div className="text-white">PreceduralAndTechnologies</div>;
+import React from 'react';
+import './Style/ProceduralAndTechnologies.css';
+
+function ProceduralAndTechnologies() {
+  return (
+    <div className='container mx-auto'>
+      <div className='box1'>Box 1</div>
+      <div className='box2'>Box 2</div>
+      <div className='box3'>Box 3</div>
+    </div>
+  );
 }
 
-export default PreceduralAndTechnologies;
+
+
+export default ProceduralAndTechnologies;
