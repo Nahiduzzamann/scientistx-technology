d55a1817db2db1c6d4e99ed578fef2d578be5b61
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-02-24 02:10:40 +0600

    ok

--- app/components/ProceduralAndTechnologies.jsx
+++ app/components/ProceduralAndTechnologies.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import './Style/ProceduralAndTechnologies.css';
+
 
 function ProceduralAndTechnologies() {
   return (
-    <div className='container mx-auto'>
-      <div className='box1'>Box 1</div>
-      <div className='box2'>Box 2</div>
-      <div className='box3'>Box 3</div>
+    <div >
+      ProceduralAndTechnologies
     </div>
   );
 }
