b40a64b768c314a0105504134111437e87ed7246
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-02-24 00:31:53 +0600

    ok

--- app/components/Partners.jsx
+++ app/components/Partners.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import img1 from "../../public/img1.png";
 import Image from "next/image";
@@ -6,18 +6,18 @@ import Svg5 from "./asset/Svg5";
 export default function Partners() {
   return (
     <div className="md:px-10 px-2 py-2">
-      <div className="flex flex-col w-full items-center">
-        <p className="text-white">Our Partners</p>
-        <div className="h-1 w-full max-w-[500px] bg-[#1B75BC]"></div>
+      <div className="flex flex-col w-full items-center py-10 md:py-16">
+        <p className="text-[20px] font-medium text-[#ffffff]">Our Partner</p>
+        <div className="h-1 my-2 w-full max-w-[300px] bg-[#1B75BC]"></div>
       </div>
-      <div className="flex md:justify-between md:flex-row flex-col justify-center my-4 md:my-10">
+      <div className="flex flex-wrap justify-center md:gap-10 gap-4 my-4 md:my-10">
         <Image src={img1} alt="partner" />
         <Image src={img1} alt="partner" />
         <Image src={img1} alt="partner" />
       </div>
-      <div className="flex flex-col w-full items-center">
-        <p className="text-white">Be a partner</p>
-        <div className="h-1 w-full max-w-[500px] bg-[#1B75BC]"></div>
+      <div className="flex flex-col w-full items-center py-10 md:py-16">
+        <p className="text-[20px] font-medium text-[#ffffff]">Be A Partner</p>
+        <div className="h-1 my-2 w-full max-w-[300px] bg-[#1B75BC]"></div>
       </div>
       <div className="flex flex-col-reverse md:flex-row md:gap-10 gap-2 justify-center items-center">
         <div>
