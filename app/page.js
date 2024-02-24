132ccb34f9376789662f703e3522c13eaf8a087a
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-02-24 20:41:29 +0600

    hjv

--- app/page.js
+++ app/page.js
@@ -24,33 +24,6 @@ const Home = () => {
       <Partners></Partners>
     </div>
   );
-  return (
-    <Parallax ref={parallaxRef} pages={6} style={{ top: "0", left: "0" }}>
-      <ParallaxLayer
-        factor={0.01}
-        offset={0.1}
-        speed={2}
-        style={{ height: "100vh" }}>
-        <div></div>
-      </ParallaxLayer>
-
-      <ParallaxLayer offset={1} speed={2.5} style={{ height: "100vh" }}>
-        <OurProject />
-      </ParallaxLayer>
-
-      <ParallaxLayer offset={2} speed={3} style={{ height: "100vh" }}>
-        <Technologies></Technologies>
-      </ParallaxLayer>
-      <ParallaxLayer offset={3} speed={1.5} style={{ height: "100vh" }}>
-        <PreceduralAndTechnologies></PreceduralAndTechnologies>
-      </ParallaxLayer>
-      <ParallaxLayer offset={4} speed={2.5} style={{ height: "100vh" }}>
-        <Partners></Partners>
-      </ParallaxLayer>
-      <ParallaxLayer offset={5} speed={1}>
-        <Footer />
-      </ParallaxLayer>
-    </Parallax>
-  );
+ 
 };
 export default Home;
